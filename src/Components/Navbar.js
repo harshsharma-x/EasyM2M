c879d162a9e64a43a0683e5efa7ae0224fccf014
Harsh Sharma <harshsharma.hs888@gmail.com>
2024-08-23 15:48:59 +0530

    NavBar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import Logo from '../Images/Logo.png'
+
+const Navbar = () => {
+  return (
+    <header className=''>
+      <nav className='flex bg-green-100 justify-around sticky top-0 z-[-20] mx-auto  w-full items-center p-4'>
+        <div>
+          <ul className='flex space-x-4 text-sm'>
+            <li>
+              <a href='#home'>Home</a>
+            </li>
+
+            <li className='flex items-center gap-2 cursor-pointer'>
+              <a href='#about'>Collection</a>
+              <i class="fa-solid fa-caret-down"></i>
+            </li>
+
+            <li className='flex items-center gap-2 cursor-pointer'>
+              <a href='#contact'>Products</a>
+              <i class="fa-solid fa-caret-down"></i>
+            </li>
+
+            <li className='flex items-center gap-2 cursor-pointer'>
+              <a href='#contact'>Other Pages</a>
+              <i class="fa-solid fa-caret-down"></i>
+            </li>
+
+            <li className='flex items-center gap-2 cursor-pointer' >
+              <a href='#contact'>Blog</a>
+              <i class="fa-solid fa-caret-down"></i>
+            </li>
+
+          </ul>
+        </div>
+
+
+        <div className='h-10 w-10 border'>
+          <img src={Logo} alt="" />
+        </div>
+
+
+        <div className='cursor-pointer'>
+
+          <ul className='flex space-x-4'>
+            <li className='flex items-center gap-2 cursor-pointer'>
+              <i class="fa-solid fa-magnifying-glass ml-4"></i>
+              <a href='#home'>Search</a>
+            </li>
+
+            <li className='flex items-center gap-2 cursor-pointer'>
+              <i class="fa-regular fa-user"></i>
+              <a href='#about'>Account</a>
+            </li>
+
+            <li className='flex items-center gap-2 cursor-pointer' >
+              <i class="fa-regular fa-heart"></i>
+              <a href='#contact'>Wishlist</a>
+            </li>
+
+            <li className='flex items-center gap-2 cursor-pointer'>
+              <i class="fa-solid  fa-cart-shopping"></i>
+              <a href='#contact'>Cart</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
