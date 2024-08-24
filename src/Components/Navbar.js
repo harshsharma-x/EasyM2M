c6eb9695b56be5ca455cf5b4c36fc39dad8aa46b
kislayrajj <kislayraj751@gmail.com>
2024-08-24 20:49:09 +0530

    done with the navbar structure

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Logo from "../assets/Images/logos/Logo.png";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [openDropDownMenu, setOpenDropDown] = useState(null);
-  const handleToggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-  const handleToggleDropDownMenu = (dropDownName) => {
-    setOpenDropDown(openDropDownMenu === dropDownName ? null : dropDownName);
-  };
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const menuRef = useRef(null);
+
   const menuLinks = [
     { title: "Home", path: "/", dropDownIcon: false },
     { title: "About", path: "/about", dropDownIcon: false },
@@ -34,87 +31,176 @@ const Navbar = () => {
     { title: "Service 3", path: "/services/service3" },
     { title: "Service 4", path: "/services/service4" },
   ];
+
+  const sidebarContents = [
+    { title: "Login", path: "/login" },
+    { title: "Register", path: "/register" },
+    { title: "Wishlist", path: "/wishlist" },
+    { title: "Checkout", path: "/checkout" },
+  ];
+  const handleToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleToggleDropDownMenu = (dropDownName) => {
+    setOpenDropDown(openDropDownMenu === dropDownName ? null : dropDownName);
+  };
+
+  const handleMouseLeave = () => {
+    setOpenDropDown(null);
+  };
+
+  const handleSidebarToggle = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+    setIsMenuOpen(false);
+  };
+  const handleClickOutside = (e) => {
+    if (
+      e.target !== menuRef?.current &&
+      !menuRef?.current?.contains(e.target)
+    ) {
+      setIsMenuOpen(false);
+      setIsSidebarOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (e) => handleClickOutside(e);
+
+    if (setIsMenuOpen || !setIsSidebarOpen) {
+      document.addEventListener("click", handleOutsideClick);
+    } else {
+      document.removeEventListener("click", handleOutsideClick);
+    }
+
+    return () => document.removeEventListener("click", handleOutsideClick); // Cleanup
+  }, [isMenuOpen, isSidebarOpen]);
+
   return (
-    <header className="sticky top-0 z-10 w-full bg-white/30 shadow-md">
-      <nav className="center justify-between p-4 mx-auto w-full max-w-7xl relative">
-        {/* Logo */}
-        <div className="h-10 w-10">
-          <img src={Logo} alt="Logo" className="w-full h-full" />
-        </div>
-
-        {/*  Menu */}
-        <div className="">
-          <ul
-            className={`flex lg:gap-12 md:gap-6 md:items-center flex-col md:flex-row md:static md:flex absolute items-start gap-6 right-[10%] top-14 bg-blue-200 md:bg-inherit px-16 py-2 md:px-0 md:py-0 rounded-lg opacity-80 md:opacity-100 ${
-              isMenuOpen ? "" : "hidden "
-            }`}>
-            {menuLinks.map((option) => {
-              const isDropDownOpen = openDropDownMenu === option.title;
-              return (
-                <li
-                  key={option.path}
-                  className="center justify-between w-full gap-1 md:gap-2 md:font-medium">
-                  <Link to={option.path}>{option.title}</Link>
-                  {option.dropDownIcon && (
-                    <div onClick={() => handleToggleDropDownMenu(option.title)} className=" cursor-pointer" title="view options">
-                      {isDropDownOpen ? (
-                        <i className="fa-solid fa-angle-up"></i>
-                      ) : (
-                        <i className="fa-solid fa-angle-down "></i>
-                      )}
-                    </div>
-                  )}
-                  {/* Dropdown for Products */}
-                  {option.title === "Products" && isDropDownOpen && (
-                    <motion.ul
-                      initial={{ opacity: 0, x: -20 }}
-                      animate={{ opacity: 1, x: 0 }}
-                      className="absolute md:top-12 top-28  -left-32 md:left-auto bg-gray-100 shadow-lg rounded-md  py-2 px-2 opacity-80">
-                      {productsLinks.map((product) => (
-                        <motion.li
-                          whileHover={{ scale: 1.02 }}
-                          key={product.path}
-                          className="list-none md:list-disc  px-4 py-2 hover:bg-gray-200 w-44">
-                          <Link to={product.path}>{product.title}</Link>
-                        </motion.li>
-                      ))}
-                    </motion.ul>
-                  )}
-                  {/* Dropdown for Services */}
-                  {option.title === "Services" && isDropDownOpen && (
-                    <motion.ul
-                      initial={{ opacity: 0, x: -20 }}
-                      animate={{ opacity: 1, x: 0 }}
-                      className="absolute md:top-12 top-40 -left-32 md:left-auto bg-gray-100 shadow-lg rounded-md  py-2 px-2 opacity-80 w-44">
-                      {servicesLinks.map((service) => (
-                        <motion.li
-                          whileHover={{ scale: 1.02 }}
-                          key={service.path}
-                          className="list-none md:list-disc  px-4 py-2 hover:bg-gray-200">
-                          <Link to={service.path}>{service.title}</Link>
-                        </motion.li>
-                      ))}
-                    </motion.ul>
-                  )}
-                </li>
-              );
-            })}
-          </ul>
-          <div
-            onClick={handleToggleMenu}
-            className="md:hidden absolute right-36 top-6 ">
-            <i className="fa-solid fa-bars"></i>
+    <header className="sticky top-0 z-10 w-full bg-white/30 shadow-md  ">
+      <div ref={menuRef} className="relative ">
+        <nav className="center justify-between p-4 mx-auto w-full max-w-7xl">
+          {/* Logo */}
+          <div className="h-10 w-10">
+            <img src={Logo} alt="Logo" className="w-full h-full" />
           </div>
-        </div>
 
-        {/* Icons */}
-        <div className=" space-x-4">
-          <i className="fa-solid fa-magnifying-glass cursor-pointer"></i>
-          <i className="fa-regular fa-heart cursor-pointer"></i>
-          <i className="fa-solid fa-cart-shopping cursor-pointer"></i>
-          <i className="fa-solid fa-sliders cursor-pointer"></i>
-        </div>
-      </nav>
+          {/*  Menu */}
+          <div className="">
+            <ul
+              onMouseLeave={() => {
+                setIsMenuOpen(false);
+              }}
+              className={`flex lg:gap-12 md:gap-6 md:items-center flex-col md:flex-row md:static md:flex absolute items-start gap-6 right-[10%] top-14 bg-blue-200 md:bg-inherit px-16 py-2 md:px-0 md:py-0 rounded-lg opacity-80 md:opacity-100 ${
+                isMenuOpen ? "" : "hidden "
+              }`}>
+              {menuLinks.map((option) => {
+                const isDropDownOpen = openDropDownMenu === option.title;
+                return (
+                  <li
+                    key={option.path}
+                    className="center justify-between w-full gap-1 md:gap-2 md:font-medium"
+                    onMouseLeave={handleMouseLeave}>
+                    <Link to={option.path}>{option.title}</Link>
+                    {option.dropDownIcon && (
+                      <div
+                        onClick={() => handleToggleDropDownMenu(option.title)}
+                        onMouseOver={() =>
+                          handleToggleDropDownMenu(option.title)
+                        }
+                        className="cursor-pointer bg-gray-100/30 size-full rounded-b-[100%] rounded-sm animate center justify-center hover:bg-white w-4"
+                        title="view options">
+                        {isDropDownOpen ? (
+                          <i className="fa-solid fa-angle-up"></i>
+                        ) : (
+                          <i className="fa-solid fa-angle-down "></i>
+                        )}
+                      </div>
+                    )}
+                    {/* Dropdown for Products */}
+                    {option.title === "Products" && isDropDownOpen && (
+                      <motion.ul
+                        initial={{ opacity: 0, x: -20 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        onMouseLeave={handleMouseLeave}
+                        className="absolute md:top-11 top-28  -left-32 md:left-auto bg-gray-100 shadow-lg rounded-md  py-2 px-2 opacity-80 font-normal">
+                        {productsLinks.map((product) => (
+                          <motion.li
+                            whileHover={{ scale: 1.02 }}
+                            key={product.path}
+                            className="px-4 py-2 hover:bg-gray-200 w-44">
+                            <Link to={product.path}>{product.title}</Link>
+                          </motion.li>
+                        ))}
+                      </motion.ul>
+                    )}
+                    {/* Dropdown for Services */}
+                    {option.title === "Services" && isDropDownOpen && (
+                      <motion.ul
+                        initial={{ opacity: 0, x: -20 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        onMouseLeave={handleMouseLeave}
+                        className="absolute md:top-12 top-40 -left-32 md:left-auto bg-gray-100 shadow-lg rounded-md  py-2 px-2 opacity-80 w-44 font-normal">
+                        {servicesLinks.map((service) => (
+                          <motion.li
+                            whileHover={{ scale: 1.02 }}
+                            key={service.path}
+                            className="px-4 py-2 hover:bg-gray-200">
+                            <Link to={service.path}>{service.title}</Link>
+                          </motion.li>
+                        ))}
+                      </motion.ul>
+                    )}
+                  </li>
+                );
+              })}
+            </ul>
+            <div
+              onClick={handleToggleMenu}
+              className="md:hidden absolute right-36 top-6 ">
+              <i className="fa-solid fa-bars"></i>
+            </div>
+          </div>
+
+          {/* Icons */}
+          <div className="flex space-x-4">
+            <div className="lg:hover:bg-gray-300 animate lg:p-1 lg:rounded-full lg:size-9 center lg:justify-center">
+              <i className="fa-solid fa-magnifying-glass cursor-pointer"></i>
+            </div>
+            <div className="lg:hover:bg-gray-300 animate lg:p-1 lg:rounded-full lg:size-9 center lg:justify-center">
+              <i className="fa-regular fa-heart cursor-pointer"></i>
+            </div>
+            <div className="lg:hover:bg-gray-300 animate lg:p-1 lg:rounded-full lg:size-9 center lg:justify-center">
+              <i className="fa-solid fa-cart-shopping cursor-pointer"></i>
+            </div>
+            <div
+              onClick={handleSidebarToggle}
+              className="lg:hover:bg-gray-300 animate lg:p-1 lg:rounded-full lg:size-9 center lg:justify-center z-20">
+              <i className="fa-solid fa-sliders cursor-pointer"></i>
+            </div>
+          </div>
+
+          {/* sidebar */}
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={isSidebarOpen ? { opacity: 0.8 } : { opacity: 0 }}
+            className={`absolute right-2 top-2 bg-white/90 h-[98vh] py-16 w-[50vw] md:w-[20vw] lg:w-[20vw] flex pl-12 rounded-md
+          ${isSidebarOpen ? "block" : "hidden"}
+          `}>
+            <ul className="flex flex-col gap-12">
+              {sidebarContents.map((option) => {
+                return (
+                  <li
+                    key={option.path}
+                    className="text-lg md:text-xl hover:text-blue-400 ">
+                    {option.title}
+                  </li>
+                );
+              })}
+            </ul>
+          </motion.div>
+        </nav>
+      </div>
     </header>
   );
 };
