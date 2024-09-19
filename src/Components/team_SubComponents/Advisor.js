d3b210ab9f35d501a20396243367f208dc958cf8
kislayrajj <kislayraj751@gmail.com>
2024-09-19 22:59:32 +0530

    card scale cousing bg jump fixed

--- src/Components/team_SubComponents/Advisor.js
+++ src/Components/team_SubComponents/Advisor.js
@@ -21,23 +21,24 @@ const Advisor = () => {
   ];
 
   return (
-    <div className="p-6 flex flex-wrap justify-center gap-8 ">
+    <div className="p-6 flex flex-wrap justify-center items-start gap-8">
       {advisors.map((advisor, index) => (
         <div
           key={index}
-          className="p-6 my-4 w-80 min-h-[435px] md:min-h-[450px] border border-gray-200 rounded-lg transform hover:scale-105 hover:z-10  transition-transform duration-300 ease-in-out flex flex-col gap-2 lg:gap-3 overflow-hidden group hover_gradient">
+          className="p-6 my-4 w-80 h-[435px] border border-gray-200 rounded-lg transform  hover:z-10 hover:scale-105 2xl:hover:scale-[1.15] transition-all duration-300 ease-in-out flex flex-col gap-2 lg:gap-3 overflow-hidden group hover_gradient 2xl:hover:mx-3">
           <img
             src={advisor?.image}
             alt={advisor?.name}
             className="w-32 h-32 rounded-full mx-auto shadow-outline-focus group-hover:shadow-outline-light group-hover:animate-oval-motion animate"
           />
-          <h2 className="mt-[5%]  text-xl font-semibold text-gray-800 text-center group-hover:animate-animateNames group-hover:w-min  group-hover:text-left">
+          <h2 className="mt-[5%]  text-xl font-semibold text-gray-800 text-center group-hover:animate-animateNames group-hover:w-min  group-hover:mt-0 group-hover:text-left">
             {advisor?.name}
           </h2>
-          <p className="mt-3 text-gray-600 text-center text-sm  font-medium group-hover:animate-animatePosition group-hover:w-min  group-hover:text-left">
+          <p className="mt-3 text-gray-600 text-center text-sm  font-medium group-hover:animate-animatePosition group-hover:w-min group-hover:mt-1  group-hover:text-left">
             {advisor?.position}
           </p>
-          <p className="mt-3 text-gray-600 text-justify text-sm group-hover:font-medium group-hover:translate-y-[-50px] transition-transform duration-700 ">
+          <p className="mt-3 text-gray-600 text-justify text-sm group-hover:font-medium group-hover:translate-y-[-50px] 
+        group-hover:mt-0 transition-transform duration-700">
             {advisor?.description}
           </p>
           <p className="mt-3 text-gray-600 text-justify text-base group-hover:font-medium group-hover:translate-y-[-50px] transition-transform duration-700 font-semibold center justify-center gap-2">
--- src/Components/team_SubComponents/Founder.js
+++ src/Components/team_SubComponents/Founder.js
@@ -8,8 +8,7 @@ const Founder = () => {
       name: "Mr PriyaRanjan Kumar",
       position: "Founder and CTO",
       description: `25+ years in Software Industry
-          Expert in IoT, M2M, NMS, Cloud, Mobile App
-          Worked in Lucent, Aricent & EGMP from IIML
+          Expert in IoT, M2M, NMS, Cloud, Mobile App Worked in Lucent, Aricent & EGMP from IIML
           B.E (Computer Science) from Bangalore Univ, India`,
       image: mrPriyaRanjanKumarDp,
     },
@@ -31,23 +30,24 @@ const Founder = () => {
   ];
 
   return (
-    <div className="p-6 flex flex-wrap justify-center gap-8  ">
+    <div className={`p-6 flex flex-wrap justify-center items-start gap-8`}>
       {founders.map((founder, index) => (
         <div
           key={index}
-          className="p-6 my-4 w-80  min-h-[435px] md:min-h-[450px] border border-gray-200 rounded-lg transform hover:scale-105 hover:z-10  transition-transform duration-300 ease-in-out flex flex-col gap-2 lg:gap-3 overflow-hidden group hover_gradient">
+          className="p-6 my-4 w-80 h-[435px] border border-gray-200 rounded-lg transform  hover:z-10 hover:scale-105 2xl:hover:scale-[1.15] transition-all duration-300 ease-in-out flex flex-col gap-2 lg:gap-3 overflow-hidden group hover_gradient 2xl:hover:mx-3">
           <img
             src={founder.image}
             alt={founder.name}
-            className="w-32 h-32 rounded-full mx-auto shadow-outline-focus group-hover:shadow-outline-light group-hover:animate-oval-motion animate"
+            className="w-32 h-32 rounded-full mx-auto shadow-outline-focus group-hover:shadow-outline-light group-hover:animate-oval-motion animate "
           />
-          <h2 className="mt-[5%]  text-xl font-semibold text-gray-800 text-center group-hover:animate-animateNames group-hover:w-min  group-hover:text-left">
+          <h2 className="mt-[5%]  text-xl font-semibold text-gray-800 text-center group-hover:animate-animateNames group-hover:w-min  group-hover:mt-0 group-hover:text-left">
             {founder.name}
           </h2>
-          <p className="mt-3 text-gray-600 text-center text-sm  font-medium group-hover:animate-animatePosition group-hover:w-min  group-hover:text-left">
+          <p className="mt-3 text-gray-600 text-center text-sm  font-medium group-hover:animate-animatePosition group-hover:w-min group-hover:mt-1  group-hover:text-left">
             {founder.position}
           </p>
-          <p className="mt-3 text-gray-600 text-justify text-sm group-hover:font-medium group-hover:translate-y-[-50px] transition-transform duration-700 ">
+          <p className="mt-3 text-gray-600 text-justify text-sm group-hover:font-medium group-hover:translate-y-[-50px] 
+        group-hover:mt-0 transition-transform duration-700">
             {founder.description}
           </p>
         </div>
